07af94b43439170c14f682859ab54ccfc45c489a
gavindinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-09-19 15:30:40 -0700

    PHP form

--- index.php
+++ index.php
@@ -4,28 +4,83 @@
 </head>
 <body>
 <?php
-  include("util.php");
- $array = array(
-      channel=>"C027GB3U3",
-      text=>"\$5, Footlongs",
-      username=>"SUBWAY",
-      attachments => '[{"pretext": "New Submission", "text": "New Submission", "color":"#fffff", "fields": [
-        {"title": "Author","value": "Gavin Dinubilo","short":false },
-        {"title": "Soundcloud Link", "value": "test", "short":true, "unfurl_links": true},
-        {"title": "Description", "value": "WOW, Zed\'s Dead is pretty cool", "short":true}
-    ]}]'
-  );
- $Slack = new Slack('xoxp-2254427554-2677262785-2687290823-213b54');
-   # print_r($Slack->call('users.list'));
-print_r($Slack->call('chanels.list'));
-echo ($Slack->call('chat.postMessage', array(
-      channel=>"C027GB3U3",
-      text=>"Soundcloud: http://soundcloud.com/zedsdead/zeds-dead-twin-shadow-lost-you-feat-dangelo-lacy",
-      username=>"SUBWAY",
-      unfurl_links=>true)));
-echo ($Slack->call('chat.postMessage', $array));
 
- 
+  include("Slack.php");  
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $fail = false;
+    if($_POST['name']) {
+      $name = $_POST['name'];
+    } else {
+      $fail = true;
+      printf("Name is required<br>");
+    }
+    if($_POST['contact']) {
+      $contact = $_POST['contact'];
+    } else {
+      $fail = true;
+      printf("Contact is required<br>"); 
+    }
+    if($_POST['song']) {
+      $song = $_POST['song'];
+    } else {
+      $fail = true;
+      printf("Song is required<br>");
+    }
+    if ($_POST['url']) {
+      $url = $_POST['url'];
+    } else {
+      $fail = true;
+      printf("Song URL is required<br>");
+    }
+    if ($_POST['social']) {
+      $social = $_POST['social'];
+    } else {
+      $social = "No social media links provided.";
+    }
+    if ($_POST['description']) {
+      $desc = $_POST['description'];
+    } else {
+      $fail = true;
+      printf("Artist description is required<br>");
+    }
+    $string = '[{"pretext": "Test", "text": "New Submission", "color":"#fffff", "fields": [
+          {"title": "Author","value": "'.$name.'\n'.$contact.'","short":false },
+          {"title": "Description","value": "'.$description.'","short":true },
+          {"title": "Social Media","value": "'.$social.'","short":true }
+          ]
+        }]';
+    if ($fail == false) {
+      $array = array(
+        channel=>"U027GCKGC",
+        text=>"\$5, Footlongs",
+        username=>"SUBWAY",
+        attachments=>$string
+      );
+      $Slack = new Slack('xoxp-2254427554-2677262785-2687290823-213b54');
+      // print_r($Slack->call('users.list'));
+      echo ($Slack->call('chat.postMessage', array(
+            channel=>"U027GCKGC",
+            text=>"Soundcloud: $url",
+            username=>"SUBWAY",
+            unfurl_links=>true)));
+      echo ($Slack->call('chat.postMessage', $array));
+    }
+  } 
 ?>
+<form id="form" action="index.php" method="post">
+  <label>Your Name*:</label><br>
+  <input type="text" id="name" name="name"><br>
+  <label>Contact Email*:</label><br>
+  <input type="email" id="email" name="contact"><br>
+  <label>Song name*:</label><br>
+  <input type="text" id="songname" name="song"><br>
+  <label>Song link*:</label><br>
+  <input type="url" id="soundcloudlink" name="url"><br>
+  <label>Description of song and/or band/artist*:</label><br>
+  <textarea id="description" maxlength="140" name="description"></textarea><br>
+  <label>Links to social media accounts:</label><br>
+  <textarea id="social" name="social"></textarea><br>
+  <input type="submit" value="Submit">
+</form>
 </body>
 </html>
