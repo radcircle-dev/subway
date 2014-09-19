d97d5a64ebbe87137d92875fcac36dbe65fbdcda
Gavin Dinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-09-19 12:07:19 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
+<?php
+  include("util.php");
+   $Slack = new Slack('xoxp-2254427554-2677262785-2687290823-213b54');
+   print_r($Slack->call('channels.list'));
+print_r($Slack->call('chat.postMessage', array(
+      channel=>"C0286H1M5",
+      text=>"\$5, Footlongs",
+      username=>"SUBWAY"
+  )));
+?>
+</body>
+</html>
