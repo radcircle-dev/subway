2f3f5e3054d5388797d1f3603638f182ee1da19b
gavindinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-09-19 15:50:23 -0700

    Added confirmation view

--- index.php
+++ index.php
@@ -13,6 +13,12 @@
       $fail = true;
       printf("Name is required<br>");
     }
+    if($_POST['artist']) {
+      $name = $_POST['artist'];
+    } else {
+      $fail = true;
+      printf("Artist Name is required<br>");
+    }
     if($_POST['contact']) {
       $contact = $_POST['contact'];
     } else {
@@ -43,7 +49,8 @@
       printf("Artist description is required<br>");
     }
     $string = '[{"pretext": "Test", "text": "New Submission", "color":"#fffff", "fields": [
-          {"title": "Author","value": "'.$name.'\n'.$contact.'","short":false },
+          {"title": "User Name","value": "'.$name.'\n'.$contact.'","short":true },
+          {"title": "Artist Name","value": "'.$artist.'","short":true },
           {"title": "Description","value": "'.$desc.'","short":true },
           {"title": "Social Media","value": "'.$social.'","short":true }
           ]
@@ -64,13 +71,17 @@
             unfurl_links=>true)));
       echo ($Slack->call('chat.postMessage', $array));
     }
-  } 
-?>
-<form id="form" action="index.php" method="post">
+    printf("<h1>Thank You</h1>");
+    printf("<p>Your submission for ".$name."'s song: ".$song." has been submitted to the Radcircle Editorial team.</p>");
+    printf("<p>We really appreciate your showing us some great music!!</p>");
+  } else {
+    printf('<form id="form" action="index.php" method="post">
   <label>Your Name*:</label><br>
   <input type="text" id="name" name="name"><br>
   <label>Contact Email*:</label><br>
   <input type="email" id="email" name="contact"><br>
+  <label>Artist Name*:</label><br>
+  <input type="text" id="artist" name="artist"><br>
   <label>Song name*:</label><br>
   <input type="text" id="songname" name="song"><br>
   <label>Song link*:</label><br>
@@ -80,6 +91,8 @@
   <label>Links to social media accounts:</label><br>
   <textarea id="social" name="social"></textarea><br>
   <input type="submit" value="Submit">
-</form>
+</form>');
+  }
+?>
 </body>
 </html>
