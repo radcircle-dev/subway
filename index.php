f413e0eec9652a51be66bfde2b8d056b4566603a
mazib <m_azib@hotmail.com>
2014-09-19 20:43:35 -0400

    styling 0.1
    first iteration

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
-<html>
-<head>
-<title>Online PHP Script Execution</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="stylesheets/style.css" rel="stylesheet">
+    <title>Online PHP Script Execution</title>
+
+
+  </head>
+
+
+
 <body>
+
 <?php
   include("Slack.php");  
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
